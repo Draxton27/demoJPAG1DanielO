3d94a85c64f8b2cd72f5a191c8298dffa83c47fe
Laboratorio C204 <labc204@uam.net.ni>
2023-10-09 18:49:39 -0600

    Initial commit

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,10 @@
+package test;
+
+import service.EntityManagerAdmin;
+
+public class Main {
+    public static void  main(String[] args) {
+        EntityManagerAdmin.getInstance();
+        System.out.println("Entre");
+    }
+}
